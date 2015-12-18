131fe361e6261f4f2459d1899cb9d4639f850546
fawzi mudawar <fawzi mudawar>
2015-12-19 00:20:33 +0300

    Select working

--- php/script.php
+++ php/script.php
@@ -8,13 +8,35 @@ function connectToDB()
 
   if(mysqli_connect_errno())
   {
-    echo "Not Connected";
+    echo "Not Connected<br>";
     exit();
   }
   else
   {
-    echo "Connected";
+    echo "Connected<br>";
   }
-}
+/*
+  //$sql = "INSERT INTO `items`(`sku`, `name`, `description`) VALUES ('fawzi','ffff','asdf')";
+  $sql = "SELECT * FROM items";
+  $result = $mysqliLink->query("SELECT * FROM items", MYSQLI_USE_RESULT)
+  if ($mysqliLink->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $mysqliLink->error;
+}*/
 
+$sql = "SELECT * FROM items";
+$result = $mysqliLink->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["item id"]. " - sku: " . $row["sku"]. " name: " . $row["name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$mysqliLink->close();
+
+}
  ?>
