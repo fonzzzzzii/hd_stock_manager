d5c6a4a49163b4423e033da049f2e9dfdc2bb488
fawzi mudawar <fawzi mudawar>
2015-12-19 11:10:48 +0300

    add_new_category added.

--- php/script.php
+++ php/script.php
@@ -1,7 +1,7 @@
 <?php
 
-add_new_item("1234","Super awesome","Something Interesting");
-
+//add_new_item("1234","Super awesome","Something Interesting");
+add_new_category("Sensors","sn");
 function connect_to_database()
 {
   $mysqliLink = new mysqli("localhost","root","","hd_stock_manager");
@@ -34,13 +34,11 @@ function add_new_item($new_sku,$new_name,$new_description)
 
   if ($result->num_rows > 0)
   {
-      $not_unique_flag = false;
       // check if new sku is unique
       while($row = $result->fetch_assoc())
       {
         if($row["sku"] == $new_sku)
         {
-          $not_unique_flag=true;
           echo '<script language="javascript">';
           echo 'alert("The entered sku (';
           echo $new_sku;
@@ -51,23 +49,57 @@ function add_new_item($new_sku,$new_name,$new_description)
           echo "id: " . $row["item id"]. " - sku: " . $row["sku"]. " name: " . $row["name"]. "<br>";
       }
 
-      //if it makes it this far, that means it's that the sku is unique
-      $sql = "INSERT INTO `items`(`sku`, `name`, `description`, `date added`) VALUES ('$new_sku','$new_name','$new_description','$new_date')";
-      //$sql = "INSERT INTO `items`(`sku`,`item id`) VALUES ('ooh',9)";
-      $result = $mysqliLink->query($sql);
-
   }
   else
   {
-      echo "0 results";
+      echo "0 results<br>";
   }
 
-  $mysqliLink->close();
+  //if it makes it this far, that means it's that the sku is unique
+  $sql = "INSERT INTO `items`(`sku`, `name`, `description`, `date added`) VALUES ('$new_sku','$new_name','$new_description','$new_date')";
+  //$sql = "INSERT INTO `items`(`sku`,`item id`) VALUES ('ooh',9)";
+  $result = $mysqliLink->query($sql);
+  echo "New item added<br>";
+
+  $mysqliLink -> close();
 }
 
-function add_new_category("new_name","new_initials")
+function add_new_category($new_name,$new_initials)
 {
-  
+  $mysqliLink = connect_to_database();
+  $sql = "SELECT * FROM categories";
+  $result = $mysqliLink->query($sql);
+  $new_date = get_now_date();
+
+  if ($result->num_rows > 0)
+  {
+      // check if new sku is unique
+      while($row = $result->fetch_assoc())
+      {
+        if($row["category initials"] == $new_initials)
+        {
+          echo '<script language="javascript">';
+          echo 'alert("The entered category initials (';
+          echo $new_initials;
+          echo  ') exist. Please enter unique initials.");';
+          echo '</script>';
+          exit();
+        }
+          echo "id: " . $row["category number"]. " - initials: " . $row["category initials"]. " name: " . $row["category name"]. "<br>";
+      }
+  }
+  else
+  {
+      echo "0 results<br>";
+  }
+
+  //if it makes it this far, that means it's that the sku is unique
+  $sql = "INSERT INTO `categories`( `date added`, `category initials`, `category name`) VALUES ('$new_date','$new_initials','$new_name')";
+  //$sql = "INSERT INTO `items`(`sku`,`item id`) VALUES ('ooh',9)";
+  $result = $mysqliLink->query($sql);
+  echo "New category added<br>";
+
+  $mysqliLink -> close();
 }
 
 
